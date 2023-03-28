8420f44e855df3bfb8eed8341529925ce78d102f
ahyk03070811 <126743881+ahyk03070811@users.noreply.github.com>
2023-03-28 12:09:29 +0000

    kadai3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
   def show
-    @user=User.find(params[:id])
-    @books=@user.books
+    @user=current_user
     @book=Book.new
+    @books=@user.books
   end
 
   def index
     @user=current_user
     @book=Book.new
+    @users=User.all
   end
 
   def edit
